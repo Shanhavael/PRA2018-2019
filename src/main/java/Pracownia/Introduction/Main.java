7d2c33d3cb85009864396f8c45ba8a807030637c
Shanhavael <konrad.skrzypczak1@op.pl>
2018-10-04 13:05:22 +0200

    commit

--- src/main/java/Pracownia/Introduction/Main.java
+++ src/main/java/Pracownia/Introduction/Main.java
@@ -1,8 +1,14 @@
 package Pracownia.Introduction;
 
+import org.apache.log4j.Logger;
+
 public class Main {
 
-    public static void main(String [ ] args) {
+    static Logger log=Logger.getLogger("abc");
+
+    public static void main(String [ ] args)
+    {
+        log.info("message");
         System.out.println("Hello World");
     }
 }
